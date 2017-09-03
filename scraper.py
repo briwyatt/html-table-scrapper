e46607b5b422d77f506d794f770b8fcc06fa0fdc
Bri Wyatt <mbriwyatt@gmail.com>
2017-09-03 05:14:41 -0500

    initial commit

---
+++ scraper.py
@@ -0,0 +1,25 @@
+import csv
+import requests
+from bs4 import BeautifulSoup
+
+url = 'http://www.showmeboone.com/sheriff/JailResidents/ JailResidents.asp'
+response = requests.get(url)
+html = response.content
+
+soup = BeautifulSoup(html, 'lxml')
+table = soup.find('table', attrs={'class': 'data-table'})
+
+list_of_rows = []
+for row in table.findAll('tr')[1:]:
+    list_of_cells = []
+
+    for cell in row.findAll('td'):
+        text = cell.text.replace('&nbsp;', '')
+        list_of_cells.append(text) 
+    list_of_rows.append(list_of_cells)
+
+outfile = open("./inmates.csv", "w")
+writer = csv.writer(outfile)
+writer.writerow(["Last", "First", "Middle", "Gender", "Race", "Age", "City", "State"])
+writer.writerows(list_of_rows)
+
